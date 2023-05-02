860d327ce71e9672bdd3fff38acad8a28379fc87
bhanu0240 <bhanureddy0240@gmail.com>
2023-05-02 08:09:08 +0530

    changed request url to https

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,10 +1,10 @@
 export const TITLE = "Splitwise";
 
-export const BASE_URL = "http://gostdlib.com/v1/";
+export const BASE_URL = "https://gostdlib.com/v1/";
 
-export const FILES_URL = "http://gostdlib.com/v1/files";
+export const FILES_URL = "https://gostdlib.com/v1/files";
 
-export const VCF_URL = "http://gostdlib.com/v1/vcf"
+export const VCF_URL = "https://gostdlib.com/v1/vcf"
 
 /* Contacts Constants*/
 
@@ -16,7 +16,7 @@ export const SEARCH_CONTACT = "Search Contacts";
 
 export const ADD_CONTACTS = "Add Contact";
 
-export const CONTACTS_URL = "http://gostdlib.com/v1/contacts";
+export const CONTACTS_URL = "https://gostdlib.com/v1/contacts";
 
 export const DEFAULT_PROFILE_ICON_HASH = "f887fc5db376ec193efb22b8eefd9464ab5547cb6f1cafebab344b8434c49506";
 
@@ -32,7 +32,7 @@ export const ENTER_GROUP_NAME = "Enter Group Name";
 
 export const SEARCH_GROUP = "Search Groups";
 
-export const GROUPS_URL = "http://gostdlib.com/v1/groups";
+export const GROUPS_URL = "https://gostdlib.com/v1/groups";
 
 export const CURRENT_GROUP_CONTACTS = "Current Group Contacts";
 
